903f9a347e171f1a8bc0be14eaeeb7a752ab3971
unizazang <zifnffk32123@gmail.com>
2023-04-11 22:07:15 +0900

    html 버전 완전 끝!!! 병합

--- js/list.js
+++ js/list.js
@@ -3,20 +3,22 @@ let layout = $(".pf-wrap").masonry({
   itemSelector: ".pf-card",
   columnWidth: ".pf-card figure",
   gutter: 54,
+
+  isResizable: true,
 });
 
-layout.on("click", ".pf-card", function (e) {
-  e.preventDefault();
+layout.on("click", ".pf-card", function () {
   $(this).toggleClass("active");
   $(this).find(".pf-thumb").toggleClass("active"); //이미지그림자 active
 
   // trigger layout after item size changes
   layout.masonry("layout");
+  // 얘 레이아웃 바꾼다음 링크가져와서 href 로 스크립트로 보내주기...
 });
 
-$(".skilltags .line-btn").filter(":first-of-type").addClass("active");
+//$(".skilltags .line-btn").filter(":first-of-type").addClass("active");
 $(".skilltags .line-btn").click(function (e) {
-  e.preventDefault();
+  // e.preventDefault();
   $(this).siblings().removeClass("active");
   $(this).toggleClass("active");
 });
