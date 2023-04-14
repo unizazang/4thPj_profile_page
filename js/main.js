0ff98f2d91a982d1ad27ea0470c0683ee8254054
unizazang <zifnffk32123@gmail.com>
2023-04-14 15:10:52 +0900

    주석 정리

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ $(window).scroll(function () {
 
 // video
 let video = $(".video-area video").get(0);
-// video.play();
 
 video.click(function () {
   $(this).prop("muted", false);
@@ -50,17 +49,6 @@ function toast(string) {
 
 // share profile
 $("button.share-profile").click(function () {
-  // let profileURL = window.location.href;
-
-  // navigator.clipboard.writeText(profileURL).then(
-  // function () {
-  // toast("링크가 클립보드에 복사되었습니다.");
-  //   },
-  //   function () {
-  //     toast("링크 복사에 실패했습니다.");
-  //   }
-  // );
-
   var url = window.location.href;
   var $temp = $("<input>");
   $("body").append($temp);
@@ -69,16 +57,3 @@ $("button.share-profile").click(function () {
   $temp.remove();
   toast("링크가 클립보드에 복사되었습니다.");
 });
-
-// TOAST 원본코드
-
-// const toast = $('#toast');
-// toast.hasClass("reveal")
-//   ? (clearTimeout(removeToast),
-//     (removeToast = setTimeout(function () {
-//       toast.removeClass("reveal");
-//     }, 1000)))
-// : (removeToast = setTimeout(function () {
-//     toast.removeClass("reveal");
-//   }, 1000));
-// toast.addClass("reveal"), (toast.text(string))
