d92ea3aa5ca9b4f2f56003e9c4fa8b6d447d4fa2
unizazang <zifnffk32123@gmail.com>
2024-05-08 16:42:31 +0900

    ㅡㅡjs올라감 그냥강력새.고가 필요했음

--- js/main.js
+++ js/main.js
@@ -54,6 +54,151 @@ $("button.share-profile").click(function () {
 });
 
 
+// ========== 20240508 추가 ===========
+
+// video
+// let video = $(".video-area video").get(0);
+
+// video.click(function () {
+//   $(this).prop("muted", false);
+// });
+
+// bg star 생성
+
+// function createStars(i) {
+//   for (var i; i; i--) {
+//     drawStars();
+//   }
+// }
+
+// function drawStars(){
+//   var tmpStar = document.createElement('figure')
+//   tmpStar.className = "star";
+//   tmpStar.style.top = 100*Math.random()+'%';
+//   tmpStar.style.left = 100*Math.random()+'%';
+//   document.getElementById('stars').appendChild(tmpStar);
+// }
+
+// function selectStars() {
+//     stars = document.querySelectorAll(".star");
+  
+// }
+
+// function animateStars() {
+//       Array.prototype.forEach.call(stars, function(el, i){
+//       TweenMax.to(el, Math.random() * 0.5 + 0.5, {opacity: Math.random(), onComplete: animateStars});
+//     });
+// }
+
+
+
+// createStars(100);
+// selectStars();
+// animateStars();
+
+
+// ------------- 다른방법
+// GASP 안쓰는걸로
+// 안됨...
+
+// var limit = 100, // Max number of stars
+//   body = document.body;
+// loop = {
+//   //initilizeing
+//   start: function() {
+//     for (var i = 0; i <= limit; i++) {
+//       var star = this.newStar();
+//       star.style.top = this.rand() * 100 + '%';
+//       star.style.left = this.rand() * 100 + '%';
+//       star.style.webkitAnimationDelay = this.rand() + 's';
+//       star.style.mozAnimationDelay = this.rand() + 's';
+//       body.appendChild(star);
+//     }
+//   },
+//   //to get random number
+//   rand: function() {
+//     return Math.random();
+//   },
+//   //createing html dom for star
+//   newStar: function() {
+//     var d = document.createElement('div');
+//     d.innerHTML =
+//       '<figure class="star"><figure class="star-top"></figure><figure class="star-bottom"></figure></figure>';
+//     return d.firstChild;
+//   },
+// };
+// loop.start();
+
+
+// ------------------- canvas, jQuery 방식 ------
+
+
+// $(document).ready(function() {
+//   $("img").sparkle({
+//   //이미지를... 넣어야 되는건가?
+//   // accepts a HEX string, or "rainbow" or an array of HEX strings:
+//   color: ["#2eafea","#e56604"],
+  
+//   // determine how many sparkles will be on the element at a time
+//   count: 30,
+  
+//   // tell the canvas how far over the edge of it's container it should overlap in pixels.
+//   overlap: 0,
+  
+//   // set the speed multiplier
+//   speed: 1,
+  
+//   // min size
+//   minSize: 4,
+  
+//   // max size
+//   maxSize: 7,
+  
+//   // "up", "down" or "both" to set which direction the sparkles will travel in.
+//   direction: "both"
+  
+//   });
+//   });
+
+
+
+
+// =============== 생성만 하고 애니메이션 없는 vanilla js  > 작동잘됨. 일단 보류 =================
+
+// (function(){
+
+//   function Stars(numberOfStars, divID){
+//     var chosenDiv = document.getElementById(divID)  
+//     chosenDiv.style.display = "none";
+//     chosenDiv.innerHTML = "";
+//     function randomFrom(array) {
+//       return array[Math.floor(Math.random() * array.length)];
+//     }
+//     var text = "";
+//     var i; 
+//     for (i = 0; i < numberOfStars; i++) {
+//         bigRange = Array.apply(null, Array(100)).map(function (_, i) {return i;});
+//         smallRange = Array.apply(null, Array(3)).map(function (_, i) {return i;});
+//         tenRange = Array.apply(null, Array(5)).map(function (_, i) {return i;});
+//         starTwinkleStage = randomFrom("9","13");
+//         var top = randomFrom(bigRange); 
+//         var right = randomFrom(bigRange); 
+//         var width = randomFrom(smallRange);
+//         text += "<style></style>";
+//         text += "<div class='stars' style='top:" + top + "%; right: "+ right +"%; width:" + width + "px; height:" + width + "px;";
+//         text += "animation: twinkling " + starTwinkleStage + "s infinite";
+//         text += ";box-shadow: 0px 0px 1vw rgba(255, 255, 255, 0.2);'></div>";
+//         chosenDiv.innerHTML = text;
+//         chosenDiv.style.display = "block";
+//     }
+// }
+  
+// // Function(How many stars, id that you want populating)
+// Stars(140, "bg-sky");
+
+// })();
+
+
 // === 새로발견한 포르투갈어 그거 !! ===
 
 function init(){
